acf9aab2dc38d6a5c11bcce6c8e577d33ac8bcf5
johnildefonsogarciaiii <116079600+johnildefonsogarciaiii@users.noreply.github.com>
2023-09-30 23:57:08 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,17 +10,10 @@ const app = express();
 app.use(express.json());
 
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "POST", "GET", "PUT", "DELETE");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// })
-
 
 app.use(cors(
     {
-    origin: '*',
+    origin: 'https://ticketing-fronend.vercel.app',
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
 }
