b788f2a8c2bca1aafe66bd692b8b69b0073448a7
johnildefonsogarciaiii <116079600+johnildefonsogarciaiii@users.noreply.github.com>
2023-10-01 17:06:53 +0800

    Update userAPI.jsx

--- client/src/api/userAPI.jsx
+++ client/src/api/userAPI.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API = axios.create({baseURL: 'https://ticketing-omega.vercel.app'})
+const API = axios.create({baseURL: 'http://127.0.0.1:5000'})
 
 export const login = (email, password) => API.post('/user/login', {email, password});
 
