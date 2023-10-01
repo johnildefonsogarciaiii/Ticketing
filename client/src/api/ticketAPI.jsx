e0deb5361e3d9248afdbf70da29a5891d395dfe5
johnildefonsogarciaiii <116079600+johnildefonsogarciaiii@users.noreply.github.com>
2023-10-01 17:06:40 +0800

    Update ticketAPI.jsx

--- client/src/api/ticketAPI.jsx
+++ client/src/api/ticketAPI.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'https://ticketing-omega.vercel.app'});
+const API = axios.create({baseURL: 'http://127.0.0.1:5000'});
 
 export const getAllTicket = () => API.get('/ticket');
 
